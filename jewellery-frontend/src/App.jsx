cd413d9ac739177993109dad30c58dd72a0b7d58
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-10-03 23:52:25 +0530

    Adding Auth Login and Register Components
    Adding Auth Login and Register Components to provide user option to register and login in the app

--- jewellery-frontend/src/App.jsx
+++ jewellery-frontend/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Login from './components/Auth/Login';
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Register from './components/Auth/Register';
+import Products from './components/App/Products';
+import Cart from './components/App/Cart';
+import Home from './components/App/Home';
+import Footer from './components/App/Footer';
+import Header from './components/App/Header'; // Import your Header component
+import AboutJewelryMaster from './components/App/About';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation(); // Get the current location
 
   return (
     <>
+      {/* Render Header only if the path is not /login or /register */}
+      {location.pathname !== '/login' && location.pathname !== '/register' && <Header /> }
+      
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path='/' element={<Home />} />
+          <Route path="/shop" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<AboutJewelryMaster />} />
+
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {location.pathname !== '/login' && location.pathname !== '/register' && <Footer /> }
+
     </>
-  )
+    
+  );
 }
 
-export default App
+// Wrap App with Router to provide routing context
+const WrappedApp = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default WrappedApp;
