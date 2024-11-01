e64f74a6814d50fe4279bf9492a0ff7727fcde55
Khush5882 <169111197+Khush5882@users.noreply.github.com>
2024-11-01 00:48:02 -0600

    Adding categories model and serializers
    Adding categories model and serializers and making model migrations

--- jewellery_shop/shop/serializers.py
+++ jewellery_shop/shop/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Order, OrderItem, Product
+from .models import Order, OrderItem, Product,ProductCategory
 from .models import Cart, CartItem, Address
 from django.contrib.auth.models import User
 from .models import UserInfo
@@ -7,10 +7,19 @@ from django.conf import settings
 from .models import JewelryCustomization
 
 
+
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = ['id', 'name', 'description']
+
 class ProductSerializer(serializers.ModelSerializer):
+    category = ProductCategorySerializer(read_only=True)  # Nested serializer to display category details
+    category_id = serializers.PrimaryKeyRelatedField(queryset=ProductCategory.objects.all(), source='category', write_only=True)  # To assign category by ID
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'stock', 'image']
+        fields = ['id', 'name', 'description', 'price', 'stock', 'image', 'category', 'category_id']
         
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
