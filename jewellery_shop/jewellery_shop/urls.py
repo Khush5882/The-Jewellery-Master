496852917b69f6ddb9385690bec95fb6cccb18ce
kulbirkaur3008 <142059272+kulbirkaur3008@users.noreply.github.com>
2024-11-17 01:43:13 +0530

    configuring urls

--- jewellery_shop/jewellery_shop/urls.py
+++ jewellery_shop/jewellery_shop/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),  # Including your shop app's urls
     path('api/', include('chatbot.urls')),
+     path("metal/", include("metal_investment.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
